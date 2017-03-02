efcad238f50fe2c7cfc39dc71c1923035d2baa27
isfonzar <icaro.isf@gmail.com>
2017-03-02 18:20:30 -0300

    Encrypting and decrypting function

--- cryptogo.go
+++ cryptogo.go
@@ -15,8 +15,10 @@ import (
 
 func main() {
 
+	fmt.Println("Encrypting")
 	encrypt()
 
+	fmt.Println("Decrypting")
 	decrypt()
 
 }
@@ -38,6 +40,8 @@ func encrypt() {
 		panic(err)
 	}
 
+	// The IV needs to be unique, but not secure. Therefore it's common to
+	// include it at the beginning of the ciphertext.
 	ciphertext := make([]byte, aes.BlockSize+len(plaintext))
 	iv := ciphertext[:aes.BlockSize]
 	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
@@ -46,6 +50,10 @@ func encrypt() {
 
 	stream := cipher.NewCFBEncrypter(block, iv)
 	stream.XORKeyStream(ciphertext[aes.BlockSize:], plaintext)
+
+	// It's important to remember that ciphertexts must be authenticated
+	// (i.e. by using crypto/hmac) as well as being encrypted in order to
+	// be secure. @todo
 
 	// create a new file for saving the encrypted data.
 	f, err := os.Create("teste.enc")
@@ -59,7 +67,7 @@ func encrypt() {
 }
 
 func decrypt() {
-	plaintext, err := ioutil.ReadFile("teste.enc")
+	ciphertext, err := ioutil.ReadFile("teste.enc")
 
 	if err != nil {
 		panic(err.Error())
@@ -70,21 +78,23 @@ func decrypt() {
 
 	dk := pbkdf2.Key(key, salt, 4096, 32, sha1.New)
 
-	fmt.Println(dk)
-
 	block, err := aes.NewCipher(dk)
 	if err != nil {
 		panic(err)
 	}
 
-	ciphertext := make([]byte, aes.BlockSize+len(plaintext))
-	iv := ciphertext[:aes.BlockSize]
-	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
-		panic(err)
+	// The IV needs to be unique, but not secure. Therefore it's common to
+	// include it at the beginning of the ciphertext.
+	if len(ciphertext) < aes.BlockSize {
+		panic("ciphertext too short")
 	}
+	iv := ciphertext[:aes.BlockSize]
+	ciphertext = ciphertext[aes.BlockSize:]
 
 	stream := cipher.NewCFBDecrypter(block, iv)
-	stream.XORKeyStream(ciphertext[aes.BlockSize:], plaintext)
+
+	// XORKeyStream can work in-place if the two arguments are the same.
+	stream.XORKeyStream(ciphertext, ciphertext)
 
 	// create a new file for saving the encrypted data.
 	f, err := os.Create("teste.dec")
